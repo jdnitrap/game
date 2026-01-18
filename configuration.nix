f21c70ff4e9fc06d704ab795f924c681a25e8b73
jdnitrap <108910755+jdnitrap@users.noreply.github.com>
2026-01-18 00:34:53 -0500

    Clean up configuration.nix by removing comments
    Removed commented-out configurations for user accounts and services.

--- configuration.nix
+++ configuration.nix
@@ -65,18 +65,9 @@
 
 
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+  
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.bob = {
-    isNormalUser = true;
-    description = "bob";
-    extraGroups = [ "networkmanager" "wheel" "input" "audio" ];
-    packages = with pkgs; [
-    
-    ];
-  };
+ 
 
   # Install firefox.
   programs.firefox.enable = true;
@@ -111,31 +102,16 @@
    
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
 
-  # List services that you want to enable:
 
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
+
+
+  
    networking.firewall.enable = true;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+ 
   system.stateVersion = "master"; #d you read the comment?
   
 
