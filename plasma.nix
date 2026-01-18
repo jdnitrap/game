c81a3f6de951d81c206e942c18b60d7ef1304ce0
jdnitrap <108910755+jdnitrap@users.noreply.github.com>
2026-01-18 01:28:41 -0500

    Enable X11 windowing system in plasma.nix

--- plasma.nix
+++ plasma.nix
@@ -2,3 +2,6 @@
 # Enable the Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
+
+ # Enable the X11 windowing system.
+  services.xserver.enable = true;
