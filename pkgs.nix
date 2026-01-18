3de7548bb81b3de2ccc4ce6098d6c9b030a9dc60
jdnitrap <108910755+jdnitrap@users.noreply.github.com>
2026-01-18 01:15:48 -0500

    Update pkgs.nix with additional packages
    Added various gaming and utility packages to system profile.

--- pkgs.nix
+++ pkgs.nix
@@ -1,1 +1,38 @@
+##pkgs.nix
 
+
+
+
+
+# Install firefox.
+  programs.firefox.enable = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #  wget
+  
+  steam
+  lutris
+  heroic
+  wine
+  linuxKernel.packages.linux_xanmod_latest.xone
+  linuxKernel.packages.linux_xanmod_latest.xpadneo
+  mangohud
+   protonup-qt 
+   linuxKernel.packages.linux_zen.xone
+	linuxKernel.packages.linux_zen.xpadneo
+	linuxKernel.packages.linux_xanmod_stable.xpadneo
+	atlauncher
+	badlion-client
+	prismlauncher
+	lunar-client
+	hmcl
+	protonvpn-gui
+	#xone
+	#xpadneo
+	git
+	bottles
+   
+  ];
