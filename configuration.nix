df7db60d9a8ea33361393a6a838b7e9d574eda71
jdnitrap <108910755+jdnitrap@users.noreply.github.com>
2026-01-18 00:35:37 -0500

    Change hostname from 'nixos' to 'gaming'

--- configuration.nix
+++ configuration.nix
@@ -20,7 +20,7 @@
   # Use latest kernel.
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "gaming"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
