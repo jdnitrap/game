e6d7f8728eeb6894832346b23a2e12f68e2f55cc
jdnitrap <108910755+jdnitrap@users.noreply.github.com>
2026-01-18 01:31:07 -0500

    Add bootloader configuration for systemd-boot

---
+++ boot.nix
@@ -0,0 +1,6 @@
+ # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
