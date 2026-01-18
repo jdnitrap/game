631059742899651cd0800c33c9218be5aa3f9434
jdnitrap <108910755+jdnitrap@users.noreply.github.com>
2026-01-18 01:09:11 -0500

    Enable automatic system updates configuration

--- autoupdate.nix
+++ autoupdate.nix
@@ -1,3 +1,10 @@
 ##Auto Update
 { config, lib, pkgs, ... }:
 {
+system.autoUpgrade = {
+  enable = true;
+  allowReboot = false;  # Allows automatic reboot if necessary
+  dates = "02:00";     # Schedule updates at 2 AM
+  randomizedDelaySec = "45min";  # Random delay to avoid simultaneous updates
+};
+}
